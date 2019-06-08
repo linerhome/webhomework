042c637ff901a3f0204c11459d9e83670e6d1f96
linerhome <2738293979@qq.com>
2019-06-08 21:55:03 +0800

    change

---
+++ react-todo/react-todo-app/backstop_data/html_report/config.js
@@ -0,0 +1,40 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Tododiff_todolist_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20190608-213537\\Tododiff_todolist_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "Tododiff_todolist_0_document_0_phone.png",
+        "label": "todolist",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8080/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "error": "Reference file not found F:\\linfile\\course\\agilityweb\\react-todo-app\\react-todo-list\\backstop_data\\bitmaps_reference\\Tododiff_todolist_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Tododiff_todolist_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20190608-213537\\Tododiff_todolist_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "Tododiff_todolist_0_document_1_tablet.png",
+        "label": "todolist",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8080/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "error": "Reference file not found F:\\linfile\\course\\agilityweb\\react-todo-app\\react-todo-list\\backstop_data\\bitmaps_reference\\Tododiff_todolist_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "Tododiff"
+});
